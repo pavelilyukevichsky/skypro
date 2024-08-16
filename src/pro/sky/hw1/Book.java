f0ccc4ea558b11fb5a38d2506b62c1e0c931151c
Pavel Iliukevich <pavel.iliukevich@resliv.by>
2024-08-16 11:57:37 +0300

    hw4(add Book.java)

---
+++ src/pro/sky/hw1/Book.java
@@ -0,0 +1,59 @@
+package pro.sky.hw1;
+
+
+
+
+public class Book {
+
+    private String name;
+    private int year;
+    private Author author;
+
+    private static int counter;
+
+    public Book() {
+        counter++;
+    }
+
+    public Book(String name, int year, Author author) {
+        this.name = name;
+        this.year = year;
+        this.author = author;
+    }
+
+    void test() {
+        String name = "sdf";
+        System.out.println(author);
+    }
+
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getCreatedBooks() {
+        return counter;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                ", author=" + author +
+                '}';
+    }
+}
